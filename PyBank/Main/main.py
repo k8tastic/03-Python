7ab1eedfa1c1eb3f6343b196ad3c0ce57edc6231
k8tastic <45125567+k8tastic@users.noreply.github.com>
2018-12-12 17:44:29 -0500

    edit

--- PyBank/Main/main.py
+++ PyBank/Main/main.py
@@ -19,9 +19,17 @@ csvpath = os.path.join("..", "Resources", "budget_data.csv")
 print ("Financial Analysis")
 print ("----------------------------")
 
-netprofit = 0
 totalmonths = 0
+totalrev = []
 monthlychange = []
+totalchange = 0
+changemonths = 0
+averagechange = 0
+maxchange = 0
+minchange = 0
+maxdate = 0
+mindate = 0
+date =[]
 
 
 with open(csvpath, "r") as csvfile:
@@ -39,39 +47,25 @@ with open(csvpath, "r") as csvfile:
     
     for row in csvreader:
         totalmonths = totalmonths + 1
-        netprofit = netprofit + int(row[1])
+        totalrev.append(float(row[1]))
+        date.append(row[0])
+        
+    for i in range(1,totalmonths):
+        monthlychange.append(totalrev[i] - totalrev[i-1])
     
+    total = int(sum(totalrev))
+    totalchange = sum(monthlychange)
+    changemonths = int(totalmonths) - 1   
+    averagechange = round(((totalchange) / (changemonths)), 2)
+        
     print (f"Total Months: {totalmonths}")
-    print (f"Total: ${netprofit}")
+    print (f"Total: ${total}")
+    print (f"Average Change: ${averagechange}")
     
-    #calc average month to month change: first calc change for each month, then take average  
-
-#    for i in range(2,len(row[1]):
-#        monthlychange.append(Profit_Losses[i] - Profit_Losses[i-1])   
-#        print (sum(monthlychange))
-#        
-#        avg_monthlychange = (sum(monthlychange)/row_count)
-
-#        max_monthlychange = max(monthlychange)
-
-#        min_monthlychange = min(monthlychange)
-
-#        max_monthlychange_date = str(date[monthlychange.index(max(monthlychange))])
-#        min_monthlychange_date = str(date[monthlychange.index(min(monthlychange))])
-      
-    #print (f"Average Change: {avg_monthlychange}")
-    #print (f"Greatest Increase in Profits: str{max_monthlychange_date}, str{max_monthlychange}")
-    #print (f"Greatest Decrease in Profits: str{min_monthlychange_date}, str{min_monthlychange}")
+    maxchange = int(max(monthlychange))
+    minchange = int(min(monthlychange))
+    maxdate = date[monthlychange.index(max(monthlychange))+1]
+    mindate = date[monthlychange.index(min(monthlychange))+1]
     
-  # The average change in "Profit/Losses" between months over the entire period
-  # The greatest increase in profits (date and amount) over the entire period
-  # The greatest decrease in losses (date and amount) over the entire period
-# As an example, your analysis should look similar to the one below:
-
-  #Average  Change: $-2315.12
-  #Greatest Increase in Profits: Feb-2012 ($1926159)
-  #Greatest Decrease in Profits: Sep-2013 ($-2196167)
-  
-
-# In addition, your final script should both print the analysis to the terminal 
-# and export a text file with the results.
+    print (f"Greatest Increase in Profits: {maxdate} (${maxchange})")
+    print (f"Greatest Decrease in Profits: {mindate} (${minchange})")
